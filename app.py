e6f2dfaa51cf67cc8a1e44324bdf326bee769778
kftlfd <kftlfd@ex.ua>
2021-10-15 20:12:31 +0300

    added Sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////trkfin.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
