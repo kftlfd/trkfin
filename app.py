9423ddc41de3d627badd3d1b23882ecb14487892
kftlfd <kftlfd@ex.ua>
2021-10-15 18:14:45 +0300

    added render_template

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "<p>Hello, World! :333</p>"
+    return render_template("index.html")
