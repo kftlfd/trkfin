1592284d71dad11def956007cfeba12a151f856d
kftlfd <kftlfd@ex.ua>
2021-10-15 20:54:53 +0300

    fixed sql

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////trkfin.db'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////trkfin.db'
+engine = create_engine('sqlite:///trkfin.db')
+db = engine.connect()
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    res = db.execute("SELECT * FROM users").all()
+    return render_template("index.html", res=res)
 
 @app.route("/register")
 def register():
