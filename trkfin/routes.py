c58bc42ca8a4897f2e91563083f760a85bf97404
kftlfd <kftlfd@ex.ua>
2021-10-18 22:06:57 +0300

    added loginForm

--- trkfin/routes.py
+++ trkfin/routes.py
@@ -1,8 +1,8 @@
-from trkfin import app
-
 from flask import redirect, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
+from trkfin import app
+from app.forms import LoginForm
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////trkfin.db'
 engine = create_engine('sqlite:///trkfin.db')
@@ -17,3 +17,8 @@ def index():
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
