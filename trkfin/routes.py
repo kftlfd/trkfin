df7276eee93b33c190c600facbb4e7277cf1c4b3
kftlfd <kftlfd@ex.ua>
2021-10-18 20:53:22 +0300

    reorganizing app to module

---
+++ trkfin/routes.py
@@ -0,0 +1,18 @@
+from trkfin import app
+from flask import redirect, render_template, request, session
+from flask_session import Session
+from sqlalchemy import create_engine
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////trkfin.db'
+engine = create_engine('sqlite:///trkfin.db')
+db = engine.connect()
+
+@app.route("/")
+@app.route("/index")
+def index():
+    res = db.execute("SELECT * FROM users").all()
+    return render_template("index.html", res=res)
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
