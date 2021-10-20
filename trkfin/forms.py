c99eb027b4cbeb35668207fc4f82c66df017206b
kftlfd <kftlfd@ex.ua>
2021-10-20 23:09:30 +0300

    added register

--- trkfin/forms.py
+++ trkfin/forms.py
@@ -1,10 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Optional, EqualTo
 
 class LoginForm(FlaskForm):
+
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('E-Mail', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[Optional()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[Optional()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message="must match")])
+    submit = SubmitField('Sign Up')
--- trkfin/models.py
+++ trkfin/models.py
@@ -2,18 +2,15 @@ from trkfin import db
 
 class Users(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    
-    username = db.Column(db.String(64), index=True, unique=True)
-    
-    email = db.Column(db.String(120), index=True, unique=True)
-    
+    id = db.Column(db.Integer, primary_key=True)    
+    username = db.Column(db.String(64), index=True, unique=True)    
+    email = db.Column(db.String(120), index=True)    
     password_hash = db.Column(db.String(128))
 
-    def __init__(self, username, email, password):
+    def __init__(self, username, email, password_hash):
         self.username = username
         self.email = email
-        self.password = password
+        self.password_hash = password_hash
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User:: id:{self.id}; username:{self.username}; email:{self.email};>'
