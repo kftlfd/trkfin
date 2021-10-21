096dc4bb26fb7b11530597d47c6e152633c04cf4
kftlfd <kftlfd@ex.ua>
2021-10-21 23:39:19 +0300

    added shell context processor

--- trkfin.py
+++ trkfin.py
@@ -1,1 +1,9 @@
-from trkfin import app
+from trkfin import app, db
+from trkfin.models import Users
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db, 'Users': Users
+    }
