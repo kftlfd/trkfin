aec918562b05361b22b6db3fd7b1abcb7932a695
kftlfd <kftlfd@ex.ua>
2021-10-25 23:04:42 +0300

    added created timestamp

--- trkfin/models.py
+++ trkfin/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -6,9 +7,10 @@ from trkfin import db, login
 
 class Users(UserMixin, db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)    
-    username = db.Column(db.String(64), index=True, unique=True)    
-    email = db.Column(db.String(120), index=True)    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True)
+    created_utc = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
