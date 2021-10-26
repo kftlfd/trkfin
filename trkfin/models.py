965c0b6f225ead409d88651bc4919f3b2b3eba5c
kftlfd <kftlfd@ex.ua>
2021-10-26 23:38:12 +0300

    preparing models

--- trkfin/models.py
+++ trkfin/models.py
@@ -12,6 +12,7 @@ class Users(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True)
     created_utc = db.Column(db.DateTime, default=datetime.utcnow)
     password_hash = db.Column(db.String(128))
+    # categories
 
     def __repr__(self):
         return f'< user | id:{self.id} | username:{self.username} | email:{self.email} >'
@@ -26,3 +27,26 @@ class Users(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return Users.query.get(int(id))
+
+
+class UserStuff(db.Model):
+
+    # user_id
+    # category
+    # amount
+
+    def __repr__(self):
+        return "stuff"
+
+
+class UsersHistory(db.Model):
+
+    # timestamp
+    # user_id
+    # type of transaction
+    # from category
+    # to category
+    # amount
+
+    def __repr__(self):
+        return "history"
