94b0ed7188773031bb6187a21c6083df14912c6f
kftlfd <kftlfd@ex.ua>
2021-11-05 22:07:06 +0200

    logging

--- trkfin/__init__.py
+++ trkfin/__init__.py
@@ -1,3 +1,6 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,3 +15,13 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from trkfin import routes, models
+
+# logging
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+file_handler = RotatingFileHandler('logs/trkfin.log', maxBytes=10240, backupCount=10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+app.logger.info('trkfin')
