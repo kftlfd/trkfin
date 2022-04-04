aec8e1853ee63d412464a3079d4fe8d108f5e26c
kftlfd <kftlfd@ex.ua>
2022-04-04 18:40:03 +0300

    administrative stuff

--- trkfin.py
+++ trkfin.py
@@ -1,7 +1,15 @@
 from trkfin import app, db
-from trkfin.models import Users
+from trkfin.models import Users, Wallets, History, Reports
 
 
+# doesn't work, no idea why. FLASK_APP is set correctly
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Users': Users}
+    symbols = {
+        'db': db,
+        'Users': Users,
+        'Wallets': Wallets,
+        'History': History,
+        'Reports': Reports
+        }
+    return symbols
