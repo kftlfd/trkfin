cd4869f6092e49278310c867b62aba3002eb9c85
kftlfd <kftlfd@ex.ua>
2022-04-09 22:51:39 +0300

    save fewer log backups

--- trkfin/__init__.py
+++ trkfin/__init__.py
@@ -28,7 +28,7 @@ if app.config['LOG_TO_STDOUT']: # Heroku env variable
     app.logger.addHandler(stream_handler)
 else: # local logs
     if not os.path.exists('logs'): os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/trkfin.log', maxBytes=10240, backupCount=10)
+    file_handler = RotatingFileHandler('logs/trkfin.log', maxBytes=10240, backupCount=3)
     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
